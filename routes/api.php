04e81bc498abc1fd8e20ddf54f237cd621f0314f
Adim Gozali <adimgozali0313@gmail.com>
2024-09-26 20:26:14 +0700

    remove auth

--- routes/api.php
+++ routes/api.php
@@ -9,20 +9,18 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::prefix('employee')->group(function () {
-        Route::get('/', [EmployeeController::class, 'index']);
-        Route::post('/store', [EmployeeController::class, 'store']);
-        Route::get('/show/{id}', [EmployeeController::class, 'show']);
-        Route::post('/update/{id}', [EmployeeController::class, 'update']);
-    });
-
-    Route::prefix('leave')->group(function () {
-        Route::get('/', [LeaveController::class, 'index']);
-        Route::post('/request', [LeaveController::class, 'requestLeave']);
-        Route::post('/manage/{id}', [LeaveController::class, 'manageLeave']);
-    });
-
-
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::prefix('employee')->group(function () {
+    Route::get('/', [EmployeeController::class, 'index']);
+    Route::post('/store', [EmployeeController::class, 'store']);
+    Route::get('/show/{id}', [EmployeeController::class, 'show']);
+    Route::post('/update/{id}', [EmployeeController::class, 'update']);
 });
+
+Route::prefix('leave')->group(function () {
+    Route::get('/', [LeaveController::class, 'index']);
+    Route::post('/request', [LeaveController::class, 'requestLeave']);
+    Route::post('/manage/{id}', [LeaveController::class, 'manageLeave']);
+});
+
+
+Route::post('/logout', [AuthController::class, 'logout']);
