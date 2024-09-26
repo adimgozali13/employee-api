1b626a4c92ee091a8db2f9ed11785fda87e90c28
Adim Gozali <adimgozali0313@gmail.com>
2024-09-26 21:21:23 +0700

    p

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -17,7 +17,7 @@ class EmployeeController extends Controller
     public function index()
     {
         try {
-            $data = User::where('role', 'employee')->orderBy('created_at','desc')->get();
+            $data = User::orderBy('created_at','desc')->get();
             return response()->json([
                 'success' => true,
                 'data' => $data,
