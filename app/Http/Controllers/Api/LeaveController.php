74a118bb65d706c7ae2b088eac0fcf1e6179f845
Adim Gozali <adimgozali0313@gmail.com>
2024-09-27 07:36:11 +0700

    p

--- app/Http/Controllers/Api/LeaveController.php
+++ app/Http/Controllers/Api/LeaveController.php
@@ -15,15 +15,15 @@ class LeaveController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index()
     {
         try {
-            $data = Leave::orderBy('created_at', 'desc');
-            $user = User::where('id',$request->user_id)->first();
-            if ($user->role == 'employee') {
-                $data = $data->where('user_id', $request->user_id);
-            }
-            $data = $data->get();
+            $data = Leave::orderBy('created_at', 'desc')->get();
+            // $user = User::where('id',$request->user_id)->first();
+            // if ($user->role == 'employee') {
+            //     $data = $data->where('user_id', $request->user_id);
+            // }
+            // $data = $data->get();
             return response()->json([
                 'success' => true,
                 'data' => $data,
